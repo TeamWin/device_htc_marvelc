373e2e35a1f93bd35c4443296d4d40477482ad0d
Andreas Schneider <asn@cryptomilk.org>
2012-06-28 15:51:11 +0200

    device: Build lights HAL.

--- device_marvelc.mk
+++ device_marvelc.mk
@@ -85,6 +85,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Add device specific modules
 PRODUCT_PACKAGES += \
+    lights.marvelc \
     camera.marvelc \
     audio.a2dp.default \
     audio.primary.marvelc \
